29ac841e89569472e2360f641abb3974f6033ca8
« Ephely » <ephely1@gmail.com>
2025-09-25 16:16:57 +0200

    Task Lockboxes

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,11 @@
+def canUnlockAll(boxes):
+    opened = set([0])
+    keys = boxes[0][:]
+
+    while keys:
+        key = keys.pop()
+        if key < len(boxes) and key not in opened:
+            opened.add(key)
+            keys.extend(boxes[key])
+
+    return len(opened) == len(boxes)
