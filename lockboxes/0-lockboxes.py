ace9917e3f58958f5b80fb51f963b064ae64f4f0
« Ephely » <ephely1@gmail.com>
2025-09-26 14:46:35 +0200

    Rules missing

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -3,10 +3,12 @@
 Write a method that determines if all the boxes can be opened.
 """
 
+
 def canUnlockAll(boxes):
     """
     You have n number of locked boxes in front of you. Each box is numbered
-    sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
+    sequentially from 0 to n - 1 and each box may contain keys to the other
+    boxes.
     """
     opened = set([0])
     keys = boxes[0][:]
