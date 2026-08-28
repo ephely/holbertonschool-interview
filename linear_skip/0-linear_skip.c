316315a16f4312611e6d3b35aefdacbbc66d27c7
« Ephely » <ephely1@gmail.com>
2026-08-28 23:55:00 +0100

    Bettystyle fixed

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -7,42 +7,46 @@
  *
  * Return: Pointer to the first node where value is located, or NULL
  */
-skiplist_t *linear_skip(skiplist_t *list, int value) {
-  skiplist_t *node, *express;
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+skiplist_t *node, *express;
 
-  if (!list)
-    return (NULL);
+if (!list)
+return (NULL);
 
-  node = list;
-  express = list->express;
+node = list;
+express = list->express;
 
-  while (express) {
-    printf("Value checked at index [%lu] = [%d]\n", express->index, express->n);
+while (express)
+{
+printf("Value checked at index [%lu] = [%d]\n", express->index, express->n);
 
-    if (express->n >= value)
-      break;
+if (express->n >= value)
+break;
 
-    node = express;
-    express = express->express;
-  }
+node = express;
+express = express->express;
+}
 
-  if (!express) {
-    express = node;
-    while (express->next)
-      express = express->next;
-  }
+if (!express)
+{
+express = node;
+while (express->next)
+express = express->next;
+}
 
-  printf("Value found between indexes [%lu] and [%lu]\n", node->index,
-         express->index);
+printf("Value found between indexes [%lu] and [%lu]\n", node->index,
+express->index);
 
-  while (node && node->index <= express->index) {
-    printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
+while (node && node->index <= express->index)
+{
+printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
 
-    if (node->n == value)
-      return (node);
+if (node->n == value)
+return (node);
 
-    node = node->next;
-  }
+node = node->next;
+}
 
-  return (NULL);
+return (NULL);
 }
